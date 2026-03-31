c1a9a8303f78697d577a771b51021be625255eea
cclee11 <cclee@wustl.edu>
2026-03-30 20:22:58 -0500

    Returns early so that it checks if the square already has an x or an o

--- tic-tac-toe/94daedf7-4c9d-4549-b46d-bfd78ae075b3/src/App.js
+++ tic-tac-toe/94daedf7-4c9d-4549-b46d-bfd78ae075b3/src/App.js
@@ -12,7 +12,10 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick() {
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
     nextSquares[i] = 'X';
