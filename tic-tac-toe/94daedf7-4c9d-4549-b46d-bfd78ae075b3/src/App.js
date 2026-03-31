83eb0db428a4d5a18600aa9792501099d5359cb3
cclee11 <cclee@wustl.edu>
2026-03-30 20:52:22 -0500

    Added some state to the Game component to track which player is next and the history of moves

--- tic-tac-toe/94daedf7-4c9d-4549-b46d-bfd78ae075b3/src/App.js
+++ tic-tac-toe/94daedf7-4c9d-4549-b46d-bfd78ae075b3/src/App.js
@@ -54,10 +54,11 @@ export default function Board() {
       </div>
     </>
   );
-  
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   return (
     <div className="game">
       <div className="game-board">
